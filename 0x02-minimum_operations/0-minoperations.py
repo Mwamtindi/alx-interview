ee8864131b8384086cb274f34c5e9244b51793a1
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-16 22:18:37 +0300

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A module with a method that calculates the fewest numb of operations """
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
