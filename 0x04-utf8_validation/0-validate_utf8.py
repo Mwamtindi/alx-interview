b84336cd7383331cd26330e8d16fe5cd7d9d34ec
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-31 21:28:18 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,28 @@
-#!/usr/bin/python3
-"""
-Module that contains a function to validate UTF-8 encoding.
-"""
+# /usr/bin/python3
+"""This module contains a function to validate UTF-8 encoding."""
 
 
 def validUTF8(data):
-    """Determine if a given data set represents a valid UTF-8 encoding."""
-    num_bytes = 0
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    """Determine if a given data set reps valid UTF-8 encoding."""
+    num_byte_s = 0
 
-    for byte in data:
-        byte = byte & 0xFF
-        if num_bytes == 0:
-            if (byte & mask1) == 0:
-                continue
-            elif (byte & (mask1 >> 1)) == mask1:
-                num_bytes = 1
-            elif (byte & (mask1 >> 2)) == (mask1 >> 1):
-                num_bytes = 2
-            elif (byte & (mask1 >> 3)) == (mask1 >> 2):
-                num_bytes = 3
+    for num in data:
+
+        if num_byte_s > 0:
+            if (num >> 6) != 0b10:
+                return False
+            num_byte_s -= 1
+        else:
+
+            if (num >> 7) == 0b0:
+                num_byte_s = 0
+            elif (num >> 5) == 0b110:
+                num_byte_s = 1
+            elif (num >> 4) == 0b1110:
+                num_byte_s = 2
+            elif (num >> 3) == 0b11110:
+                num_byte_s = 3
             else:
                 return False
-        else:
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-            num_bytes -= 1
 
-    return num_bytes == 0
+    return num_byte_s == 0
